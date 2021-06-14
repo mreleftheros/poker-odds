dc9574e35c092c2d535219245568bff71409477f
eleftheros <eleftheros_all@protonmail.com>
2021-06-14 07:40:36 +0300

    Create players

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,21 @@ const welcome = document.getElementById("welcome");
 const poker = document.getElementById("poker");
 const startBtn = document.getElementById("startBtn");
 const endBtn = document.getElementById("endBtn");
+const players = [];
 
 // create Player class
-class Person {
+class Player {
   constructor() {
     this.call = null;
     this.raise = null;
   }
+}
+
+// create players and push them to players array
+for(let i = 0; i < 9; i++) {
+  const player = new Player();
+
+  players.push(player);
 }
 
 // function to make welcome screen disappear
