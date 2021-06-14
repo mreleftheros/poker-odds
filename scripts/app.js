fddcd28d3195d46b1ea8aa15d7251129f3801174
eleftheros <eleftheros_all@protonmail.com>
2021-06-14 07:35:31 +0300

    Create Person Class

--- scripts/app.js
+++ scripts/app.js
@@ -3,20 +3,28 @@ const poker = document.getElementById("poker");
 const startBtn = document.getElementById("startBtn");
 const endBtn = document.getElementById("endBtn");
 
+// create Player class
+class Person {
+  constructor() {
+    this.call = null;
+    this.raise = null;
+  }
+}
+
 // function to make welcome screen disappear
 const closeWelcome = () => {
   welcome.classList.add("move");
 
   welcome.addEventListener("transitionend", e => {
     e.target.style.display = "none";
-    init();
   });
 };
 
-// function to initialiaze UI
-const init = () => {
+// function to end current round
+const endRound = () => {
 
 };
 
-// events
+// event listeners
 // startBtn.addEventListener("click", closeWelcome);
+endBtn.addEventListener("click", endRound);
