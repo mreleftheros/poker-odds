321bd4a4abbdd8c60685d1b1c086d45a967c8dc7
eleftheros <eleftheros_all@protonmail.com>
2021-06-14 08:28:02 +0300

    Update Player Class

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,9 @@ const welcome = document.getElementById("welcome");
 const poker = document.getElementById("poker");
 const startBtn = document.getElementById("startBtn");
 const endBtn = document.getElementById("endBtn");
+const endMessage = document.getElementById("endMessage");
 const players = [];
+let totalRounds = 0;
 
 // create Player class
 class Player {
@@ -11,6 +13,14 @@ class Player {
     this.callScore = 0;
     this.raise = null;
     this.raiseScore = 0;
+  }
+
+  updateCallScore() {
+    if(this.call) return this.callScore++;
+  }
+
+  updateRaiseScore() {
+    if(this.raise) return this.raiseScore++;
   }
 }
 
@@ -40,9 +50,38 @@ const checkAllValues = () => {
   return true;
 };
 
+// function to update endMessage UI
+const updateEndMessage = value => {
+  if(value) {
+    endMessage.textContent = "Round Ended Successfully!";
+    endMessage.classList.add("success");
+
+    setTimeout(() => {
+      endMessage.classList.remove("success");
+    }, 2000)
+  }
+  else {
+    endMessage.textContent = "Sorry! Round cannot end!";
+    endMessage.classList.add("error");
+
+    setTimeout(() => {
+      endMessage.classList.remove("error");
+    }, 2000)
+  }
+};
+
 // function to end current round
 const endRound = () => {
   let allChecked = checkAllValues(); // returns true or false
+
+  if(allChecked) {
+    totalRounds++;
+    updateEndMessage(true);
+  }
+  else {
+    updateEndMessage(false);
+  }
+
 };
 
 // event listeners
